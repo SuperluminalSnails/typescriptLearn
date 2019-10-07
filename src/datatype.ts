0b42ab68ce4ab6ed9ff8a0c54301cc60f4c83609
devman <devman@influx.io>
2019-10-08 02:21:59 +0800

    “typescript的类型”

---
+++ src/datatype.ts
@@ -0,0 +1,49 @@
+// 原始类型
+let bool: boolean = true;
+let num: number | undefined | null = 123;
+let str: string = 'abc';
+
+// 数组
+let arr1: number[] = [1, 2, 3];
+let arr2: Array<number | string> = [1, 2, 3, '4'];
+
+// 元组类型
+let tuple: [number, string] = [1, '2'];
+
+// 函数
+let add = (x: number, y: number) => x + y;
+let computed: (x: number, y: number) => number;
+
+computed = (a, b) => a + b;
+
+// 对象
+let obj: { x: number; y: number } = { x: 1, y: 2 };
+obj.x = 3;
+
+// sysmol
+let s1: symbol = Symbol();
+let s2 = Symbol();
+// console.log(s1 === s2);
+
+// undefined null
+let un: undefined = undefined;
+let nu: null = null;
+
+num = un;
+
+// void
+let noReturn = () => {};
+
+// any
+let x = 1;
+// x = '123';
+// x = () => {};
+
+// never
+let error = () => {
+  throw new Error('error');
+};
+
+let endless = () => {
+  while (true) {}
+};
