a654cdc5f4319ccdb1e63522a8ca664632c09b8c
devman <devman@influx.io>
2019-10-11 22:30:06 +0800

    “接口”

---
+++ src/interface.ts
@@ -0,0 +1,26 @@
+interface List {
+  id: number;
+  name: string;
+}
+
+interface Result {
+  data: List[];
+}
+function render(result: Result) {
+  result.data.forEach(value => {
+    console.log(value.id, value.name);
+  });
+}
+
+let result = {
+  data: [{ id: 1, name: 'A' }, { id: 2, name: 'B' }]
+};
+
+interface StringArray {
+  [index: number]: string;
+}
+
+let chars: StringArray = ['A', 'B'];
+
+// render(result);
+console.log(chars);
