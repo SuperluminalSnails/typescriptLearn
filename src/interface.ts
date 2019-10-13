24e23ee4f2637c71425cfe90190a0b9fa53ab5f3
devman <devman@influx.io>
2019-10-13 15:04:22 +0800

    “接口”

--- src/interface.ts
+++ src/interface.ts
@@ -1,26 +1,54 @@
-interface List {
-  id: number;
-  name: string;
+// interface List {
+//   id: number;
+//   name: string;
+// }
+
+// interface Result {
+//   data: List[];
+// }
+// function render(result: Result) {
+//   result.data.forEach(value => {
+//     console.log(value.id, value.name);
+//   });
+// }
+
+// let result = {
+//   data: [{ id: 1, name: 'A' }, { id: 2, name: 'B' }]
+// };
+
+// interface StringArray {
+//   [index: number]: string;
+// }
+
+// let chars: StringArray = ['A', 'B'];
+
+// // render(result);
+// console.log(chars);
+
+// interface Add {
+//   (x: number, y: number): number;
+// }
+
+// // type Add = (x: number, y: number) => number;
+
+// let intefaceadd: Add = (a, b) => a + b;
+
+interface Lib {
+  (): void;
+  version: string;
+  doSomething(): void;
 }
 
-interface Result {
-  data: List[];
-}
-function render(result: Result) {
-  result.data.forEach(value => {
-    console.log(value.id, value.name);
-  });
+function getLib() {
+  let lib: Lib = (() => {}) as Lib;
+  lib.version = '1.0';
+  lib.doSomething = () => {};
+  return lib;
 }
 
-let result = {
-  data: [{ id: 1, name: 'A' }, { id: 2, name: 'B' }]
-};
+let l1 = getLib();
+let l2 = getLib();
 
-interface StringArray {
-  [index: number]: string;
-}
-
-let chars: StringArray = ['A', 'B'];
-
-// render(result);
-console.log(chars);
+l2.version = '3.0';
+console.log(l1.version);
+console.log(l2.version);
